3b88f00735134528cabaf0bbf683fc2d2bb1c54d
keemaan <keemaan@naver.com>
2021-08-31 12:41:17 +0900

    페이지 로드 코드 삭제

--- app/src/main/java/com/example/miritestwebapp_20210823/MainActivity.kt
+++ app/src/main/java/com/example/miritestwebapp_20210823/MainActivity.kt
@@ -3,14 +3,30 @@ package com.example.miritestwebapp_20210823
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebSettings
+
+import android.webkit.WebViewClient
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myWebView: WebView = findViewById(R.id.webView)
-        myWebView.loadUrl("http://miritest.net")
+        val mWebView : WebView = findViewById(R.id.webView)
+        val mWebSettings = mWebView.getSettings()
+
+        mWebView.setWebViewClient(WebViewClient())
+
+        mWebSettings.setJavaScriptEnabled(true)
+        mWebSettings.setLoadWithOverviewMode(true)
+        mWebSettings.setUseWideViewPort(true)
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN)
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE)
+        mWebSettings.setDomStorageEnabled(true)
+        mWebView.loadUrl("http://miritest.net")
     }
 
 }
