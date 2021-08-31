b037c25c1abdb3a9326ba35f4fca2e918cf2d871
keemaan <keemaan@naver.com>
2021-08-31 13:11:13 +0900

    webViewActivity 추가 초기화면 버튼

--- app/src/main/java/com/example/miritestwebapp_20210823/MainActivity.kt
+++ app/src/main/java/com/example/miritestwebapp_20210823/MainActivity.kt
@@ -1,32 +1,26 @@
 package com.example.miritestwebapp_20210823
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebSettings
 
 import android.webkit.WebViewClient
-
-
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mWebView : WebView = findViewById(R.id.webView)
-        val mWebSettings = mWebView.getSettings()
+        startBtn.setOnClickListener {
 
-        mWebView.setWebViewClient(WebViewClient())
+            val myIntent = Intent(this,WebViewActivity::class.java)
+            startActivity(myIntent)
 
-        mWebSettings.setJavaScriptEnabled(true)
-        mWebSettings.setLoadWithOverviewMode(true)
-        mWebSettings.setUseWideViewPort(true)
-        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN)
-        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE)
-        mWebSettings.setDomStorageEnabled(true)
-        mWebView.loadUrl("http://miritest.net")
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/example/miritestwebapp_20210823/WebViewActivity.kt
@@ -0,0 +1,28 @@
+package com.example.miritestwebapp_20210823
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+        val mWebView : WebView = findViewById(R.id.webView)
+        val mWebSettings = mWebView.getSettings()
+
+        mWebView.setWebViewClient(WebViewClient())
+
+        mWebSettings.setJavaScriptEnabled(true)
+        mWebSettings.setLoadWithOverviewMode(true)
+        mWebSettings.setUseWideViewPort(true)
+        mWebSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN)
+        mWebSettings.setCacheMode(WebSettings.LOAD_NO_CACHE)
+        mWebSettings.setDomStorageEnabled(true)
+        mWebView.loadUrl("http://miritest.net")
+
+    }
+}
